629259245ba1511549f7dfec3d9cc66779d8ab32
glebskotnikov <skotnikov.nsk@gmail.com>
2024-08-10 16:26:46 -0700

    Add Payment model, related views, serializers, and admin integration

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,12 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
+
+
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
 
 
 class CourseSerializer(ModelSerializer):
@@ -9,7 +15,13 @@ class CourseSerializer(ModelSerializer):
         fields = "__all__"
 
 
-class LessonSerializer(ModelSerializer):
+class CourseDetailSerializer(ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+    lessons_count = SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
     class Meta:
-        model = Lesson
-        fields = "__all__"
+        model = Course
+        fields = ("name", "description", "lessons_count", "lessons")
--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import User
+from users.models import User, Payment
+
+
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
 
 
 class UserSerializer(ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ('email', 'phone', 'city', 'avatar')
+        fields = ("email", "phone", "city", "avatar", "payments")
